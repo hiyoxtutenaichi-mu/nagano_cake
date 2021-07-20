f61a818575d7135d413c594dcf277695804c9fef
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-20 16:49:51 +0000

    orderの途中です

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,16 @@ class Order < ApplicationRecord
 
   enum payment_method: { クレジットカード: 0, 銀行振り込み: 1 }
 
-  belongs_to :user
-  has_many :order_details
+  belongs_to :customer
+  has_many :order_detail
+  has_many :items, through: :order_details
+  
+  def total_price
+    array = []
+    self.order_details.each do |order_detail|
+       array << order_detail.price * order_detail.quantity
+   end
+    array.sum
+  end
 
 end
