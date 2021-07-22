5c58211baf4d6f35c15a0b140dc5ac3ab381e37f
terada0628 <84890597+terada0628@users.noreply.github.com>
2021-07-22 13:32:34 +0900

    Update registrations_controller.rb

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -57,6 +57,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
-  #   customer_path(resource)
+  #   super(resource)
   # end
 end
