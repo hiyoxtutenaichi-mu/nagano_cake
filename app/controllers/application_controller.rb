c1b693e5d7d767a75e5b1a56bb3d26af68a74e52
terada0628 <84890597+terada0628@users.noreply.github.com>
2021-07-24 15:44:10 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,15 +29,15 @@ class ApplicationController < ActionController::Base
   end
 
 
-  # def after_sign_out_path_for(resource_or_scope)
-  #   if resource_or_scope == :member
-  #     root_path
-  #   elsif resource_or_scope == :admin
-  #     new_admin_session_path
-  #   else
-  #     root_path
-  #   end
-  # end
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :member
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
 
   # def after_sign_in_path_for(resource)
   #   customers_path
