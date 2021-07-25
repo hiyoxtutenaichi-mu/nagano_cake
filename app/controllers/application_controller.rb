590a4fc930a8e9e08824be7f76f81ef098fe37b5
takuma25 <t.o12.13@icloud.com>
2021-07-25 02:28:36 +0000

    end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
-  
+
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :member
@@ -31,7 +30,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    customers_path
+      case resource
+      when Customer
+        customers_path
+      when Admin
+        admin_root_path
+      end
   end
-
 end
